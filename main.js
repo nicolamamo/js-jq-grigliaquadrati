ed9ac7151f1cba3e35291e75a71a36944a5f6f4c
nicolamamo <62040103+nicolamamo@users.noreply.github.com>
2020-05-07 18:07:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 
+for (var i = 0; i < 25; i++) {
+    $('#griglia').append('<div class="quadrato"></div>');
+}
 
-$(document).ready(function(){
-  $("#barre").click(function(){
-    $("#sandwich").toggle();
-  });
-});
+function assegna_numero(){
+    $( ".quadrato" ).each(function(){
+        var numero = genera_random(1,10)
+        console.log(numero);
+        $(this).append(numero);
+    });
+
+}
+assegna_numero();
+
+
+
+
+
+
+function genera_random (min, max){
+return Math.floor(Math.random() *(max-min+1)) + min;
+}
